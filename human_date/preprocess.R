5db0a65e500d9de5823814b895f29b1143d2ec1f
technicakidz <perfumemarch08@yahoo.co.jp>
2017-06-08 00:16:44 +0900

    use knn

---
+++ human_date/preprocess.R
@@ -0,0 +1,34 @@
+#load to data
+#dt:datasets, label:users(30), act:each of activity labels(6)
+library(caret)
+
+d <- data.frame(read.table("/Users/yoshidatakayuki/Data/uci_har_hg/train/X_train.txt"))
+act <- data.frame(read.table("/Users/yoshidatakayuki/Data/uci_har_hg/train/y_train.txt"))
+class <- data.frame(read.table("/Users/yoshidatakayuki/Data/uci_har_hg/train/subject_train.txt"))
+
+class$V1 = factor(class$V1)
+colnames(class)[1] = "label"
+
+models = list()
+trControl = trainControl(method = 'repeatedcv',
+                         number = 2,
+                         repeats = 1)
+
+#preProcess = NULL
+print("Making knn model...")
+
+models$knn <- train(d,class$label, method = 'knn',  tuneGrid = expand.grid(k=c(3,5)),
+                    metric = 'Kappa',
+                    trControl = trControl)
+
+print("Making lda model...")
+models$lda <- train(d,class$label,method = 'lda',
+                    preProcess = preProcess, metric = 'Kappa', trControl = trControl)
+
+print("Making rf model...")
+models$rf <- train(d, class$label, method = 'rf', tuneGrid = expand.grid(mtry = 2),
+                   preProcess = preProcess, metric = 'Kappa', trControl = trControl)#,
+
+print("Making svmRadial model...")
+models$svmRadial <- train(d, class$label, method = 'svmRadial', tuneGrid = expand.grid(sigma=c(0.014,0.034), C=10),
+                          preProcess = preProcess, metric = 'Kappa', trControl = trControl)#,
