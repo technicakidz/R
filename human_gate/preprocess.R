1506d7ec5fb28e281b61cf73ff88a5ee684f77ae
technicakidz <perfumemarch08@yahoo.co.jp>
2017-06-08 17:58:35 +0900

    fix_1

--- human_gate/preprocess.R
+++ human_gate/preprocess.R
@@ -1,26 +1,35 @@
 #load to data
 #dt:datasets, label:users(30), act:each of activity labels(6)
 library(caret)
+library(doParallel)
 
 d <- data.frame(read.table("/Users/yoshidatakayuki/Data/uci_har_hg/train/X_train.txt"))
 act <- data.frame(read.table("/Users/yoshidatakayuki/Data/uci_har_hg/train/y_train.txt"))
 class <- data.frame(read.table("/Users/yoshidatakayuki/Data/uci_har_hg/train/subject_train.txt"))
 
-colnames(class)[1] = "label"
+#userとlabelの固定ラベルラベル作成 ex.)u1_walk
 
-colnames(act)[1] = "act"
+#colnames(class)[1] = "label"
+
+#colnames(act)[1] = "act"
 #ラベルの連結
-data = cbind(d, class)
-data = cbind(data, act)
+#data = cbind(d, class)
+#data = cbind(data, act)
 
 class$V1 = factor(class$V1)
+colnames(class)[1] = "label"
 
-#userとlabelの固定ラベルラベル作成 ex.)u1_walk
+#並列化
+t<-proc.time()
+
+cl <- makeCluster(detectCores()) 
+
+registerDoParallel(cl)
 
 models = list()
 trControl = trainControl(method = 'repeatedcv',
-                         number = 2,
-                         repeats = 1)
+                         number = 10,
+                         repeats = 10)
 
 #preProcess = NULL
 print("Making knn model...")
@@ -31,12 +40,13 @@ models$knn <- train(d,class$label, method = 'knn',  tuneGrid = expand.grid(k=c(3
 
 print("Making lda model...")
 models$lda <- train(d,class$label,method = 'lda',
-                    preProcess = preProcess, metric = 'Kappa', trControl = trControl)
+                    metric = 'Kappa', trControl = trControl)
 
-print("Making rf model...")
-models$rf <- train(d, class$label, method = 'rf', tuneGrid = expand.grid(mtry = 2),
-                   preProcess = preProcess, metric = 'Kappa', trControl = trControl)#,
 
 print("Making svmRadial model...")
 models$svmRadial <- train(d, class$label, method = 'svmRadial', tuneGrid = expand.grid(sigma=c(0.014,0.034), C=10),
-                          preProcess = preProcess, metric = 'Kappa', trControl = trControl)#,
+                          metric = 'Kappa', trControl = trControl)
+
+stopCluster(cl)
+
+proc.time()-t
